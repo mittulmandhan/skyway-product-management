df06dab131c1092d6044b1c61710d4103a6ef9ad
mittulmandhan <conanmittul@gmail.com>
2020-06-12 02:47:04 +0530

    product module updated

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -3,10 +3,10 @@ import { CommonModule } from '@angular/common';
 import { CreateProductComponent } from './create-product/create-product.component';
 import { ProductRoutingModule } from './product-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
 
 import {MatInputModule} from '@angular/material/input';
-import {MatGridListModule, } from '@angular/material/grid-list';
+import {MatGridListModule } from '@angular/material/grid-list';
 import {MatCardModule} from '@angular/material/card';
 
 
